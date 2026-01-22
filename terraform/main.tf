e0d84d6a11ee4f6a39618af53389cc7e1587ca61
Rohith M <rohith9m@gmail.com>
2026-01-22 11:27:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
+resource "aws_dynamodb_table" "requests_table" {
+  name         = "${var.project_name}-${var.environment}-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "request_id"
+
+  attribute {
+    name = "request_id"
+    type = "S"
+  }
+}
+
+resource "aws_sqs_queue" "request_queue" {
+  name = "${var.project_name}-${var.environment}-queue"
+}
+
+resource "aws_sns_topic" "notifications" {
+  name = "${var.project_name}-${var.environment}-notifications"
+}
 
