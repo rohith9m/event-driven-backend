50185943e12a80def48c49e6f1993e608e11c5fb
Rohith M <rohith9m@gmail.com>
2026-01-22 11:49:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,4 +16,81 @@ resource "aws_sqs_queue" "request_queue" {
 resource "aws_sns_topic" "notifications" {
   name = "${var.project_name}-${var.environment}-notifications"
 }
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-${var.environment}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+resource "aws_iam_policy" "lambda_policy" {
+  name = "${var.project_name}-${var.environment}-lambda-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = aws_sqs_queue.request_queue.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem"
+        ]
+        Resource = aws_dynamodb_table.requests_table.arn
+      },
+      {
+        Effect = "Allow"
+        Action = "sns:Publish"
+        Resource = aws_sns_topic.notifications.arn
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+resource "aws_lambda_function" "processor" {
+  function_name = "${var.project_name}-${var.environment}-processor"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.9"
+
+  filename         = "lambda/handler.zip"
+  source_code_hash = filebase64sha256("lambda/handler.zip")
+
+  environment {
+    variables = {
+      TABLE_NAME    = aws_dynamodb_table.requests_table.name
+      SNS_TOPIC_ARN = aws_sns_topic.notifications.arn
+    }
+  }
+}
+resource "aws_lambda_event_source_mapping" "sqs_trigger" {
+  event_source_arn = aws_sqs_queue.request_queue.arn
+  function_name    = aws_lambda_function.processor.arn
+  batch_size       = 5
+  enabled          = true
+}
 
