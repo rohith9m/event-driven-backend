f8efd43d042a56741bb88c59f06c60e7665e2a2e
Rohith M <rohith9m@gmail.com>
2026-01-22 11:27:34 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "dynamodb_table" {
+  value = aws_dynamodb_table.requests_table.name
+}
+
+output "sqs_queue" {
+  value = aws_sqs_queue.request_queue.name
+}
+
+output "sns_topic" {
+  value = aws_sns_topic.notifications.name
+}
 
